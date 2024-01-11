c7974e5d913b55baa6187fad5611026960ec6710
Abdulaziz-elitecoder <abdulaziz.muhammed.elnoti@gmail.com>
2024-01-11 05:26:45 +0200

    updated routes with edit and show pages

--- routes/articles.js
+++ routes/articles.js
@@ -6,6 +6,11 @@ router.get('/new', (req, res) => {
     res.render('articles/new', {article: new Article()})
 })
 
+router.get('/edit/:id', async (req, res) => {
+    const article = await Article.findById(req.params.id)
+    res.render('articles/edit', { article: article })
+  })
+
 router.get('/:slug', async (req, res) => {
     const article =await Article.findOne({slug : req.params.slug } )
     if (article == null) {
@@ -14,27 +19,52 @@ router.get('/:slug', async (req, res) => {
     res.render('articles/show.ejs', {article: article})
 })
 
-router.post('/', async (req, res) => {
-    console.log(req.body); // Log the received data to check if it's correct
+// router.post('/', async (req, res) => {
+//     console.log(req.body); // Log the received data to check if it's correct
 
-    let article = new Article({
-        title: req.body.title,
-        description: req.body.description,
-        markdown: req.body.markdown,
-    })
-    try {
-        article = await article.save();
-        console.log('Article saved successfully:', article);
-        res.redirect(`/articles/${article.slug}`)
-    } catch (err) {
-        console.error('Error saving article:', err);
-        res.render('articles/new', { article: article });
-      }
-    });
+//     let article = new Article({
+//         title: req.body.title,
+//         description: req.body.description,
+//         markdown: req.body.markdown,
+//     })
+//     try {
+//         article = await article.save();
+//         console.log('Article saved successfully:', article);
+//         res.redirect(`/articles/${article.slug}`)
+//     } catch (err) {
+//         console.error('Error saving article:', err);
+//         res.render('articles/new', { article: article });
+//       }
+//     });
+
+router.post('/', async (req, res, next) => {
+    req.article = new Article()
+    next()
+  }, saveArticleAndRedirect('new'))
+  
+  router.put('/:id', async (req, res, next) => {
+    req.article = await Article.findById(req.params.id)
+    next()
+  }, saveArticleAndRedirect('edit'))
 
 router.delete('/:id', async (req, res) => {
     await Article.findByIdAndDelete(req.params.id)
     res.redirect('/');
 })
+
+function saveArticleAndRedirect(path) {
+    return async (req, res) => {
+      let article = req.article
+      article.title = req.body.title
+      article.description = req.body.description
+      article.markdown = req.body.markdown
+      try {
+        article = await article.save()
+        res.redirect(`/articles/${article.slug}`)
+      } catch (e) {
+        res.render(`articles/${path}`, { article: article })
+      }
+    }
+  }
 
 module.exports = router
